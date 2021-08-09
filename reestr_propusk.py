744a9bc411a19833c0c634b40e85f4255563c361
galashov <igalashow@gmail.com>
2021-08-09 14:11:53 +0300

    add ActionChains

--- reestr_propusk.py
+++ reestr_propusk.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
 import time
 import requests
 from fake_useragent import UserAgent
